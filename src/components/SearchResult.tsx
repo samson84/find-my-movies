df6f5192930fd2a445cd53753c21621ff2bc788a
Daniel Hatas <samson84@gmail.com>
2022-03-24 20:26:52 +0100

    Fine tune search results styling, add genres

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -4,11 +4,32 @@ import {
   Card,
   CardContent,
   Link,
-  Grid,
+  Box,
+  Chip,
 } from "@mui/material";
 import React from "react";
 import { Movie } from "../api/tmdb";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
+
+const Score = ({ score }: { score: number }) => (
+  <Stack alignItems="strech" direction="row">
+    <StarBorderIcon fontSize="small" />
+    <T variant="body1" component="span">
+      {score}
+    </T>
+  </Stack>
+);
+
+type GenresProp = {
+  genres: Movie["genres"];
+};
+const Genres = ({ genres }: GenresProp) => (
+  <Box component="span">
+    {genres.map((genre) => (
+      <Chip variant="outlined" label={genre.name} size="small" />
+    ))}
+  </Box>
+);
 
 type ItemProps = {
   movie: Movie;
@@ -17,18 +38,20 @@ type ItemProps = {
 const Item = ({ movie, onTitleClick }: ItemProps) => (
   <Card elevation={2}>
     <CardContent>
-      <Link
-        variant="body1"
-        component="button"
-        onClick={() => onTitleClick(movie.name)}
-      >
-        {movie.name}
-      </Link>
-      <Stack alignItems="strech" direction="row">
-        <StarBorderIcon fontSize="small" />
-        <T variant="body1" component="span">
-          {movie.score}
-        </T>
+      <Stack spacing={1}>
+        <Box>
+          <Link
+            variant="body1"
+            component="button"
+            onClick={() => onTitleClick(movie.name)}
+          >
+            <T>{movie.name}</T>
+          </Link>
+        </Box>
+        <Stack direction="row" spacing={1}>
+          <Score score={movie.score} />
+          <Genres genres={movie.genres} />
+        </Stack>
       </Stack>
     </CardContent>
   </Card>
